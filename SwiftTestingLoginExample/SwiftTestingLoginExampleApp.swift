6b6dd354a26959f0e88ce246907f14a9ceb50e81
Melisa Gülgün Dombaycı <melissagulgun@gmail.com>
2024-07-31 21:09:44 +0300

    Initial Commit

---
+++ SwiftTestingLoginExample/SwiftTestingLoginExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingLoginExampleApp.swift
+//  SwiftTestingLoginExample
+//
+//  Created by Melisa Gülgün on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingLoginExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTestingLoginExampleTests/SwiftTestingLoginExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingLoginExampleTests.swift
+//  SwiftTestingLoginExampleTests
+//
+//  Created by Melisa Gülgün on 31.07.2024.
+//
+
+import Testing
+@testable import SwiftTestingLoginExample
+
+struct SwiftTestingLoginExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
